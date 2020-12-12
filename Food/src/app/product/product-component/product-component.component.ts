893d502bc4347afd9a20ed110a5827705acab262
jmaiiomar <60675567+jmaiiomar@users.noreply.github.com>
2020-12-12 20:45:48 +0100

    commit

--- Food/src/app/product/product-component/product-component.component.ts
+++ Food/src/app/product/product-component/product-component.component.ts
@@ -44,19 +44,19 @@ export class ProductComponentComponent implements OnInit {
 
   }
   ngOnInit(): void {
-    
 
-    if(localStorage.getItem('Panier')!=null)
-    {JSON.parse(localStorage['Panier']).forEach(element => {
 
-      this.ListProductpanier.push(element['produit']);
-    });}
-    else
-    {
-      this.ListProductpanier=[];
+    if (localStorage.getItem('Panier') != null) {
+      JSON.parse(localStorage['Panier']).forEach(element => {
+
+        this.ListProductpanier.push(element['produit']);
+      });
     }
-   
-    this.x=false;
+    else {
+      this.ListProductpanier = [];
+    }
+
+    this.x = false;
     /**//////////////////////// */
     this.route.paramMap.subscribe(params => {
       this.id = params.get('categorieId');
@@ -77,7 +77,7 @@ export class ProductComponentComponent implements OnInit {
         this.ListProduct = data;
         let i = this.ListProduct.length;
         this.ListProduct.forEach(element => {
-           if (element.categorieProduct['categorie'][0]['id'] === this.c[0]['id']) {
+          if (element.categorieProduct['categorie'][0]['id'] === this.c[0]['id']) {
             let prd = new Product();
             prd = element;
             this.ListProduct2.push(prd);
@@ -91,70 +91,72 @@ export class ProductComponentComponent implements OnInit {
   }
 
   command(product: Product) {
-
     let data = this.myform.value.quantite;
-    let panier = new CommandeProduit();
-    panier.produit = product;
-    panier.prix=product.prix*data;
-    panier.quantite = data;
-    //console.log(panier)
-    if(localStorage.getItem('Panier')!=null)
-    {JSON.parse(localStorage['Panier']).forEach(element => {
-      if (this.Commande.find((test) => test.produit.id === element['produit'].id) == undefined) {
-        this.Commande.push(element);
-            }
-      
 
-    });
-    console.log(this.Commande)
-  }
-  if (this.Commande.find((test) => test.produit.id === panier.produit.id) === undefined) {
-    this.Commande.push(panier);
-  }
- 
-  
-    localStorage.setItem('Panier', JSON.stringify(this.Commande));
-    this.toastr.success('Added Succes!', 'Panier ');
-    JSON.parse(localStorage['Panier']).forEach(element => {
+    if (data != "") {
+      let panier = new CommandeProduit();
+      panier.produit = product;
+      panier.prix = product.prix * data;
+      panier.quantite = data;
+      //console.log(panier)
+      if (localStorage.getItem('Panier') != null) {
+        JSON.parse(localStorage['Panier']).forEach(element => {
+          if (this.Commande.find((test) => test.produit.id === element['produit'].id) == undefined) {
+            this.Commande.push(element);
+          }
+        });
+        console.log(this.Commande)
+      }
+      if (this.Commande.find((test) => test.produit.id === panier.produit.id) === undefined) {
+        this.Commande.push(panier);
+      }
 
-      this.ListProductpanier.push(element['produit']);
-    });
+
+      localStorage.setItem('Panier', JSON.stringify(this.Commande));
+      this.toastr.success('Added Succes!', 'Panier ');
+      JSON.parse(localStorage['Panier']).forEach(element => {
+
+        this.ListProductpanier.push(element['produit']);
+      });
     //localStorage['Panier'].push(this.Commande);
     /* var a = [];
      a.push(panier)
      a.push(JSON.parse(localStorage.getItem('Panier')));
-     localStorage.setItem('Panier', JSON.stringify(a));*/
-  }
- 
-  ProduitExistePanier(p: Product): boolean {
-   
-     if(this.ListProductpanier.find(e => e.id === p.id)==null)
-     {
-       return false;
-     }
-     else 
-     return true ;
-  }
-  recherche()
-  {if (this.search !== '') {
-
-     this.ListProduct2.forEach(element=>{
-      if(element.name===this.search || element.description===this.search  || element.prix===this.search)
-      this.ListProduct2=[];
-      this.ListProduct2.push(element);
-    }
-    )}
+     localStorage.setItem('Panier', JSON.stringify(a));*/}
     else
-  { this.ListProduct2=[];
-    this.ListProduct.forEach(element => {
-    if (element.categorieProduct['categorie'][0]['id'] === this.c[0]['id']) {
-      let prd = new Product();
-      prd = element;
-      this.ListProduct2.push(prd);
-    }
+      this.toastr.error('You have to add Quantite!', 'Panier ');
 
-  });
-}
+  }
+
+  ProduitExistePanier(p: Product): boolean {
+
+    if (this.ListProductpanier.find(e => e.id === p.id) == null) {
+      return false;
+    }
+    else
+      return true;
+  }
+  recherche() {
+    if (this.search !== '') {
+
+      this.ListProduct2.forEach(element => {
+        if (element.name === this.search || element.description === this.search || element.prix === this.search)
+          this.ListProduct2 = [];
+        this.ListProduct2.push(element);
+      }
+      )
+    }
+    else {
+      this.ListProduct2 = [];
+      this.ListProduct.forEach(element => {
+        if (element.categorieProduct['categorie'][0]['id'] === this.c[0]['id']) {
+          let prd = new Product();
+          prd = element;
+          this.ListProduct2.push(prd);
+        }
+
+      });
+    }
   }
 
 }
